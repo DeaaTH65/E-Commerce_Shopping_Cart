75ffbbbf261cb2228145ea3d9f1e9c9f13341d19
Deaath65 <deaath65@gmail.com>
2023-07-21 13:30:06 +0545

    authcart finished

--- ecommerse/ecommerse/settings.py
+++ ecommerse/ecommerse/settings.py
@@ -110,8 +110,8 @@ USE_TZ = True
 # sending emails
 
 EMAIL_HOST='smtpout.secureserver.net'
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSWORD=''
+EMAIL_HOST_USER='your_email'
+EMAIL_HOST_PASSWORD='your_password'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
