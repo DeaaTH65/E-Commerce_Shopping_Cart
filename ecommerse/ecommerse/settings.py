465b474eacb38c44c57ab25bd3d15a43ca4060d5
Deaath65 <deaath65@gmail.com>
2023-07-21 12:58:59 +0545

    update password

--- ecommerse/ecommerse/settings.py
+++ ecommerse/ecommerse/settings.py
@@ -107,6 +107,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+# sending emails
+
+EMAIL_HOST='smtpout.secureserver.net'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
