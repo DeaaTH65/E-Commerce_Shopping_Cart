fe7bba547867304336cc02ce213fb1e43733cec8
Deaath65 <deaath65@gmail.com>
2023-07-21 12:33:10 +0545

    login user

--- ecommerse/authcart/views.py
+++ ecommerse/authcart/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth.models import User
+from django.views.generic import View
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+
 
 
 # Create your views here.
 def signup(request):
     return render(request, 'authentication/signup.html')
 
+
 def handlelogin(request):
-    return render(request, 'authentication/login.html')
+    if request.method=="POST":
+
+        username=request.POST['email']
+        userpassword=request.POST['pass1']
+        myuser=authenticate(username=username,password=userpassword)
+
+        if myuser is not None:
+            login(request,myuser)
+            messages.success(request,"Login Success")
+            return redirect('/')
+
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect('/auth/login')
+
+    return render(request,'login.html')
 
 def handlelogout(request):
     return redirect('handlelogin')
