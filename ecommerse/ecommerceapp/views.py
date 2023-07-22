92048ce6881f0224090e8e62739eb85f942b58de
Deaath65 <deaath65@gmail.com>
2023-07-22 11:42:54 +0545

    profile

--- ecommerse/ecommerceapp/views.py
+++ ecommerse/ecommerceapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 
@@ -13,3 +14,26 @@ def contact(request):
 
 def about(request):
     return render(request,"about.html")
+
+
+def profile(request):
+    if not request.user.is_authenticated:
+        messages.warning(request,"Login & Try Again")
+        return redirect('/auth/login')
+    currentuser=request.user.username
+    items=Orders.objects.filter(email=currentuser)
+    rid=""
+    for i in items:
+        print(i.oid)
+        # print(i.order_id)
+        myid=i.oid
+        rid=myid.replace("ShopyCart","")
+        print(rid)
+    status=OrderUpdate.objects.filter(order_id=int(rid))
+    for j in status:
+        print(j.update_desc)
+
+   
+    context ={"items":items,"status":status}
+    # print(currentuser)
+    return render(request,"profile.html",context)
