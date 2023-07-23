c8a21f7a921c049b3f1f139ca49859d8c35c3c86
Deaath65 <deaath65@gmail.com>
2023-07-23 08:26:37 +0545

    Orders started

--- ecommerse/ecommerceapp/admin.py
+++ ecommerse/ecommerceapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from ecommerceapp.models import Product, Contact
+from ecommerceapp.models import Product, Contact, Orders
 
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Contact)
+admin.site.register(Orders)
