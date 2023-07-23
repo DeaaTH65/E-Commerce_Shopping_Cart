5d2e571cb5d7e370dfe0318e97561da302f283e0
Deaath65 <deaath65@gmail.com>
2023-07-23 08:44:04 +0545

    Checkout

--- ecommerse/ecommerceapp/views.py
+++ ecommerse/ecommerceapp/views.py
@@ -61,3 +61,29 @@ def profile(request):
     context ={"items":items,"status":status}
     # print(currentuser)
     return render(request,"profile.html",context)
+
+
+def checkout(request):
+    if not request.user.is_authenticated:
+        messages.warning(request,"Login & Try Again")
+        return redirect('/auth/login')
+
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        amount = request.POST.get('amt')
+        email = request.POST.get('email', '')
+        address1 = request.POST.get('address1', '')
+        address2 = request.POST.get('address2','')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        Order = Orders(items_json=items_json,name=name,amount=amount, email=email, address1=address1,address2=address2,city=city,state=state,zip_code=zip_code,phone=phone)
+        print(amount)
+        Order.save()
+        update = OrderUpdate(order_id=Order.order_id,update_desc="the order has been placed")
+        update.save()
+        thank = True
+        
+    return render(request, 'checkout.html')
